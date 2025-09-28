90a91c7380255b9470ba84381058bc05b60a4946
Jeremy Kubica <104161096+jeremykubica@users.noreply.github.com>
2025-09-28 16:06:55 -0400

    Change how ParameterizedNodes access

--- src/lightcurvelynx/base_models.py
+++ src/lightcurvelynx/base_models.py
@@ -166,6 +166,29 @@ class ParameterSource:
         self.value = param_name
 
 
+class AttributeNode:
+    """A class to wrap a single attribute of an object."""
+
+    def __init__(self, attr_name, parent):
+        self.attr_name = attr_name
+        self.parent = parent
+
+    def __call__(self, graph_state):
+        """Return the value in the graph_state of this attribute.
+
+        Parameters
+        ----------
+        graph_state : GraphState
+            The current graph state.
+
+        Returns
+        -------
+        any
+            The value of the attribute in the graph state.
+        """
+        return graph_state[self.parent.node_string][self.attr_name]
+
+
 class ParameterizedNode:
     """Any model that uses parameters that can be set by constants,
     functions, or other parameterized nodes.
@@ -400,19 +423,16 @@ class ParameterizedNode:
             value = kwargs[name]
 
         if callable(value):
-            if "__self__" in value.__dir__() and isinstance(value.__self__, ParameterizedNode):
-                # Case 1a: This is a method attached to another ParameterizedNode.
-                # Check if this is a getter method, including one that might have been automatically
-                # created in add_parameter(). If this is a getter, we access the parameter's sampled
-                # values directly to save a function call.
-                method_name = value.__name__
-                parent = value.__self__
-                if method_name in parent.setters:
-                    self.setters[name].set_as_parameter(parent, method_name)
+            if isinstance(value, AttributeNode):
+                # Case 1a: This is an attribute of a ParameterizedNode.
+                # We set the parameter's value in this node as the extraction of the
+                # parameter's value in the parent node.
+                if value.attr_name in value.parent.setters:
+                    self.setters[name].set_as_parameter(value.parent, value.attr_name)
                 else:
                     raise ValueError(
-                        f"Trying to set parameter '{name}' to the {type(parent)}.{method_name}, "
-                        f"but unable to find that parameter in class {type(parent)}."
+                        f"Trying to set parameter '{name}' to the {type(value.parent)}.{value.attr_name}, "
+                        f"but unable to find that parameter in class {type(value.parent)}."
                     )
             else:
                 # Case 1b: This is a general function or callable method from another object.
@@ -502,22 +522,14 @@ class ParameterizedNode:
         if allow_gradient is not None:
             self.setters[name].allow_gradient = allow_gradient
 
-        # Create a callable getter function with the same name as the parameter.
-        # This function allows us to reference the parameter as object.parameter_name
+        # Create an AttributeNode to represent this parameter.
+        # This node allows us to reference the parameter as object.parameter_name
         # for chaining without copying the value. For example, if my_node_1, is a
         # ParameterizedNode with a parameter x, we can do:
         #   my_node_2 = ParameterizedNode(y=my_node_1.x)
         # and my_node_2 will know to use the sampled values of x from my_node_1
         # (as opposed to the setter for x).
-        #
-        # We override the __self__ and __name__ attributes so it looks like method of
-        # this object and the assignment y=my_node_1.x doesn't do a copy of the value.
-        def getter(graph_state):
-            return graph_state[getter.__self__.node_string][getter.__name__]
-
-        getter.__self__ = self
-        getter.__name__ = name
-        setattr(self, name, getter)
+        setattr(self, name, AttributeNode(name, self))
 
     def compute(self, graph_state, rng_info=None, **kwargs):
         """Placeholder for a general compute function, which is called at the end
