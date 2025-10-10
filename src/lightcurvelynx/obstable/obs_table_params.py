fd0c87e5f12cae532e9c813ec25d868460da4efe
Jeremy Kubica <104161096+jeremykubica@users.noreply.github.com>
2025-10-10 15:20:02 -0400

    First version

---
+++ src/lightcurvelynx/obstable/obs_table_params.py
@@ -0,0 +1,209 @@
+"""This is a file with helper functions for deriving different parameters from an ObsTable."""
+
+import logging
+
+import numpy as np
+
+from lightcurvelynx.consts import GAUSS_EFF_AREA2FWHM_SQ
+
+
+class _ParamFormula:
+    """Class to formulas for deriving parameters.
+
+    Attributes
+    ----------
+    parameter : str
+        The name of the parameter.
+    inputs : list of str
+        The names of the input parameters required for the formula.
+    formula : function
+        The function that computes the parameter from the inputs.
+    """
+
+    def __init__(self, parameter, inputs, formula):
+        self.parameter = parameter
+        self.inputs = inputs
+        self.formula = formula
+
+    def can_solve(self, params):
+        """Check if the formula can be solved with the given parameters.
+
+        Parameters
+        ----------
+        params : dict
+            A dictionary mapping parameter names to their values.
+
+        Returns
+        -------
+        bool
+            True if the formula can be solved, False otherwise.
+        """
+        return all(param in params and params[param] is not None for param in self.inputs)
+
+    def solve(self, params):
+        """Call the formula with the given input parameters.
+
+        Parameters
+        ----------
+        params : dict
+            A dictionary mapping parameter names to their values.
+
+        Returns
+        -------
+        The value of the derived parameter.
+        """
+        logging.getLogger(__name__).debug(f"Deriving parameter {self.parameter} from {self.inputs}")
+        return self.formula(**{k: params[k] for k in self.inputs})
+
+
+class _ParamDeriver:
+    """Class to derive parameters from an ObsTable using predefined formulas.
+
+    Attributes
+    ----------
+    parameters : dict
+        A dictionary mapping parameter names to their values. With None for parameters
+        that do not have a valid value yet.
+    formulas : list of _ParamFormula
+        A list of _ParamFormula instances for deriving parameters.
+    org_parameters : set of str
+        A set of parameter names that are original parameters from the ObsTable.
+
+    Supported parameters (and their units) include:
+    - airmass: Airmass (unitless)
+    - dark_current: Dark current in electrons / second / pixel
+    - exptime: Exposure time in seconds
+    - ext_coeff: Extinction coefficient in mag / airmass
+    - fwhm_px: Full-width at half-maximum of the PSF in pixels
+    - maglim: Limiting magnitude (5-sigma) in mag
+    - nexposure: Number of exposures per observation (unitless)
+    - pixel_scale: Pixel scale in arcseconds per pixel
+    - psf_footprint: Effective footprint of the PSF in pixels^2
+    - read_noise: Read noise in electrons
+    - seeing: Seeing in arcseconds
+    - sky: Sky background in electrons / pixel^2
+    - skybrightness: Sky brightness in mag / arcsec^2
+    - zp: Instrumental zero point (nJy per electron)
+
+    """
+
+    def __init__(self):
+        self.parameters = {
+            "airmass": None,  # Airmass (unitless)
+            "dark_current": None,  # Dark current in electrons / second / pixel
+            "exptime": None,  # Exposure time in seconds
+            "ext_coeff": None,  # Extinction coefficient in mag / airmass
+            "fwhm_px": None,  # Full-width at half-maximum of the PSF in pixels
+            "maglim": None,  # Limiting magnitude (5-sigma) in mag
+            "nexposure": None,  # Number of exposures per observation (unitless)
+            "pixel_scale": None,  # Pixel scale in arcseconds per pixel
+            "psf_footprint": None,  # Effective footprint of the PSF in pixels
+            "read_noise": None,  # Read noise in electrons
+            "seeing": None,  # Seeing in arcseconds
+            "sky": None,  # Sky background in electrons / pixel^2
+            "skybrightness": None,  # Sky brightness in mag / arcsec^2
+            "zp": None,  # Instrumental zero point (nJy per electron)
+        }
+        self.formulas = []
+        self.org_parameters = set()
+        self._init_formulas()
+
+    def add_formula(self, parameter, inputs, formula):
+        """Add a new formula to the list of formulas.
+
+        Parameters
+        ----------
+        parameter : str
+            The name of the parameter to be derived.
+        inputs : list of str
+            The names of the input parameters required for the formula.
+        formula : function
+            The function that computes the parameter from the inputs.
+        """
+        # Make sure all the input and output parameters are in the parameters dict.
+        if parameter not in self.parameters:
+            raise KeyError(f"Parameter {parameter} is not a supported parameter.")
+        for input_param in inputs:
+            if input_param not in self.parameters:
+                raise KeyError(f"Parameter {input_param} is not a supported parameter.")
+
+        # Append the formula itself.
+        self.formulas.append(_ParamFormula(parameter, inputs, formula))
+
+    def init_from_obs_table(self, obs_table):
+        """Initialize parameters from an ObsTable.
+
+        Parameters
+        ----------
+        obs_table : ObsTable
+            The observation table from which to initialize parameters.
+        """
+        # Get the filter row so we can unpack dictionaries if needed.
+        filters = obs_table["filter"]
+
+        # Load each parameter from the ObsTable if it exists and is valid.
+        for param in self.parameters:
+            if param in obs_table.columns:
+                values = obs_table[param]
+            elif param in obs_table.survey_values:
+                values = obs_table.survey_values[param]
+                if type(values) is dict:
+                    values = np.array([values.get(band, None) for band in filters])
+            else:
+                values = None
+
+            # Only use the values if they are valid (not None or NaN).
+            if values is not None and not np.any(np.isnan(values) | (values == None)):  # noqa: E711
+                self.parameters[param] = values
+                self.org_parameters.add(param)
+
+    def derive_parameters(self, obs_table):
+        """Derive parameters from an ObsTable using the predefined formulas.
+
+        Parameters
+        ----------
+        obs_table : ObsTable
+            The observation table from which to derive parameters.
+        """
+        self.init_from_obs_table(obs_table)
+
+        # Keep iterating through the formulas, trying to solve them, until we do a full iteration
+        # through all of them without making any new progress.
+        made_progress = True
+        while made_progress:
+            made_progress = False
+            for formula in self.formulas:
+                if self.parameters[formula.parameter] is None and formula.can_solve(self.parameters):
+                    self.parameters[formula.parameter] = formula.solve(self.parameters)
+                    made_progress = True
+
+        # Update the ObsTable with the derived parameters. Do not overrwrite anything already
+        # there. Always prefer to add scalars to the survey values over adding entire columns.
+        for param, value in self.parameters.items():
+            if param not in self.org_parameters and value is not None:
+                if np.isscalar(value):
+                    obs_table.survey_values[param] = value
+                elif len(np.unique(value)) == 1:
+                    obs_table.survey_values[param] = value[0]
+                else:
+                    obs_table.add_column(param, value)
+
+    def _init_formulas(self):
+        """Initialize the standard formulas for deriving parameters."""
+
+        # Formulas for deriving the psf_footprint (in pixels^2) and related information.
+        self.add_formula(
+            parameter="psf_footprint",
+            inputs=["fwhm_px"],
+            formula=lambda fwhm_px: GAUSS_EFF_AREA2FWHM_SQ * fwhm_px**2,
+        )
+        self.add_formula(
+            parameter="fwhm_px",
+            inputs=["pixel_scale", "seeing"],
+            formula=lambda pixel_scale, seeing: seeing / pixel_scale,
+        )
+        self.add_formula(
+            parameter="fwhm_px",
+            inputs=["psf_footprint"],
+            formula=lambda psf_footprint: np.sqrt(psf_footprint / GAUSS_EFF_AREA2FWHM_SQ),
+        )
