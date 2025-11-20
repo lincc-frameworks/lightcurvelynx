134694ac06e8ddad3dec5a766fe891890896fd47
Jeremy Kubica <104161096+jeremykubica@users.noreply.github.com>
2025-11-20 15:59:39 -0500

    Update test_white_noise.py

--- tests/lightcurvelynx/effects/test_white_noise.py
+++ tests/lightcurvelynx/effects/test_white_noise.py
@@ -13,17 +13,43 @@ def test_white_noise() -> None:
     # We can apply the noise.
     values = np.full((5, 3), 100.0)
     values = white_noise.apply(values, white_noise_sigma=0.1)
-    assert not np.all(values == 100.0)
+    assert len(np.unique(values)) == 15
     assert np.all(np.abs(values - 100.0) <= 1.0)
 
     # We can override the default value using the parameters.
     values = white_noise.apply(values, white_noise_sigma=20.0)
-    assert not np.all(values == 100.0)
+    assert len(np.unique(values)) == 15
     assert not np.all(np.abs(values - 100.0) <= 1.0)
 
     # We fail if we do not pass in the expected parameters.
     with pytest.raises(ValueError):
         _ = white_noise.apply(values)
+
+
+def test_white_noise_seed() -> None:
+    """Test that we can sample and create a WhiteNoise object with a given seed."""
+    white_noise = WhiteNoise(white_noise_sigma=0.1)
+
+    # We can apply the noise.
+    values1 = np.full((5, 3), 100.0)
+    values1 = white_noise.apply(values1, white_noise_sigma=0.1, white_noise_seed=100)
+
+    # If we don't use the same seed, we get different values.
+    values2 = np.full((5, 3), 100.0)
+    values2 = white_noise.apply(values2, white_noise_sigma=0.1, white_noise_seed=101)
+    assert not np.allclose(values1, values2)
+
+    # If we do use the same seed, we get the same values.
+    values3 = np.full((5, 3), 100.0)
+    values3 = white_noise.apply(values3, white_noise_sigma=0.1, white_noise_seed=100)
+    assert np.allclose(values1, values3)
+
+    # A seed of None gives us different values each time.
+    values4 = np.full((5, 3), 100.0)
+    values4 = white_noise.apply(values4, white_noise_sigma=0.1)
+    values5 = np.full((5, 3), 100.0)
+    values5 = white_noise.apply(values5, white_noise_sigma=0.1)
+    assert not np.allclose(values4, values5)
 
 
 def test_white_noise_bandflux() -> None:
@@ -77,6 +103,12 @@ def test_constant_sed_model_white_noise() -> None:
     values2 = model.evaluate_sed(times, wavelengths, state, rng_info=np.random.default_rng(100))
     assert not np.any(values1 == 10.0)
     assert np.all(values1 == values2)
+
+    # If we sample the parameters multiple times, we should get the same sigma but
+    # different seeds for each model.
+    mult_states = model.sample_parameters(num_samples=1_000)
+    assert np.all(mult_states["my_constant_sed_model"]["white_noise_sigma"] == 0.1)
+    assert len(np.unique(mult_states["my_constant_sed_model"]["white_noise_seed"])) >= 950
 
 
 def test_constant_sed_model_white_noise_obs_frame() -> None:
