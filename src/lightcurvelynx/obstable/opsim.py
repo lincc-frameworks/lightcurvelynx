3685dda73e4c88cacfb61f3d74bc47532179b0d8
Jeremy Kubica <104161096+jeremykubica@users.noreply.github.com>
2025-12-04 13:25:07 -0500

    Update opsim.py

--- src/lightcurvelynx/obstable/opsim.py
+++ src/lightcurvelynx/obstable/opsim.py
@@ -211,6 +211,11 @@ class OpSim(ObsTable):
     def from_ccdvisit_table(cls, table, make_detector_footprint=False, **kwargs):
         """Construct an OpSim object from a CCDVisit table.
 
+        As an example we could access the DP1 CCDVisit table from RSP as:
+            from lsst.rsp import get_tap_service
+            service = get_tap_service("tap")
+            table = service.search("SELECT * FROM dp1.CcdVisit").to_table().to_pandas()
+
         Parameters
         ----------
         table : pandas.core.frame.DataFrame
