2c4e718f3fda5a351151cd353579ce0582ba6338
Jeremy Kubica <104161096+jeremykubica@users.noreply.github.com>
2025-12-18 13:23:38 -0500

    Update pylima_models.py

--- src/lightcurvelynx/models/pylima_models.py
+++ src/lightcurvelynx/models/pylima_models.py
@@ -3,6 +3,8 @@
 https://github.com/ebachelet/pyLIMA
 """
 
+import warnings
+
 import numpy as np
 from citation_compass import CiteClass
 
@@ -17,7 +19,7 @@ def _load_pylima_model_class(model_name):
     Parameters
     ----------
     model_name : str
-        The name of the bagle model class to load.
+        The name of the pyLIMA model type to load. E.g., 'PSPL'
 
     Returns
     -------
@@ -31,7 +33,11 @@ def _load_pylima_model_class(model_name):
             "The pyLIMA package is required to use the PyLIMAWrapperModel. Please install it. "
             "See https://pylima.readthedocs.io/en/latest/ for instructions."
         ) from err
-    model_class = getattr(models, model_name)
+
+    # The package and the class uses different naming conventions with the
+    # package having '_model' suffix and the class having 'model' suffix.
+    model_package = getattr(models, f"{model_name}_model")
+    model_class = getattr(model_package, f"{model_name}model")
     return model_class
 
 
@@ -52,13 +58,13 @@ class PyLIMAWrapperModel(BandfluxModel, CiteClass):
         The name of the pyLIMA model class to use in the simulation or the class itself.
     source_mags : dict
         A mapping from filter names to source magnitudes for the microlensed source.
-    blend_mags : dict, optional
-        A mapping from filter names to blend magnitudes for the microlensed source.
+    pylima_model_params : dict, optional
+        A dictionary of additional pyLIMA parameters for the model, such as
+        'u0', 'tE', 'rho', etc. If a parameter is already added to the model,
+        its value will be updated instead.
     parallax_model : str, optional
         The pyLIMA parallax model type: 'None', 'Annual', 'Terrestrial', or 'Full'.
-    blend_flux_parameter : str,
-        The pyLIMA blend flux parameter type: 'fblend', 'gblend', 'ftotal', or
-        or 'noblend'
+        The times for the parallax are automatically set during the evaluation.
     **kwargs : dict, optional
         Any additional keyword arguments.
 
@@ -77,8 +83,8 @@ class PyLIMAWrapperModel(BandfluxModel, CiteClass):
         self,
         model_info,
         source_mags,
-        blend_mags=None,
         *,
+        pylima_params=None,
         parallax_model="None",
         blend_flux_parameter="noblend",
         **kwargs,
@@ -97,16 +103,14 @@ class PyLIMAWrapperModel(BandfluxModel, CiteClass):
                 Mag2FluxNode(mag),
             )
 
-        # Add the blend fluxes if provided, otherwise set to zero.
-        if blend_mags is not None:
-            for filter_name, mag in blend_mags.items():
-                self.add_parameter(
-                    f"fblend_{filter_name}",
-                    Mag2FluxNode(mag),
-                )
-        else:
-            for filter_name in self.filters:
-                self.add_parameter(f"fblend_{filter_name}", 0.0)
+        # Add any of the pyLIMA parameters from the pylima_params dictionary.
+        if pylima_params is not None:
+            for name, value in pylima_params.items():
+                if name not in self.setters:
+                    self.add_parameter(name, value)
+                else:
+                    warnings.warn(f"Parameter {name} already exists in the model. Overriding its value.")
+                    self.set_parameter(name, value)
 
         # Save the model class and create a model object. We allow the user
         # to pass in a class to simplify testing when PyLIMA is not installed.
@@ -115,19 +119,22 @@ class PyLIMAWrapperModel(BandfluxModel, CiteClass):
         else:
             self._model_class = model_info
 
-        # Create a dummy pyLIMA model instance to get the parameter names.
-        # Add any missing parameters to this model, trying to pull their setters
-        # from kwargs.
-        event = self.make_pylima_event(ra=0.0, dec=0.0, filter="r", times=np.array([0.0, 1.0]))
+        # Create a dummy pyLIMA model instance to get the parameter names and
+        # check that they are all added.
+        event = self.make_pylima_event(ra=0.0, dec=0.0, filter="r", times=np.array([60676.0]))
         model = self._model_class(
             event,
-            self.blend_flux_parameter,
-            parallax=[self.parallax_model, 0.0],
+            parallax=[self.parallax_model, 60676.0 + MJD_OFFSET],
+            blend_flux_parameter=self.blend_flux_parameter,
+            double_source=self.double_source,
+            orbital_motion=self.orbital_motion,
+            origin=self.origin,
+            fancy_parameters=self.fancy_parameters,
         )
 
         expected_params_map = model.pyLIMA_standards_dictionnary
         for name in expected_params_map:
-            if name not in self._parameter_names:
+            if name not in self.setters:
                 if name in kwargs:
                     self.add_parameter(name, kwargs[name])
                 else:
@@ -136,7 +143,7 @@ class PyLIMAWrapperModel(BandfluxModel, CiteClass):
                         f"but this was not provided as an argument or added as a model parameter."
                     )
 
-    def make_pylima_event(self, ra, dec, filter=None, times=None):
+    def make_pylima_emakevent(self, ra, dec, filter=None, times=None):
         """Create a pyLIMA event object and attach a telescope if filter and times are given.
 
         Parameters
@@ -173,11 +180,6 @@ class PyLIMAWrapperModel(BandfluxModel, CiteClass):
             pylima_event.telescopes.append(tel)
 
         return pylima_event
-
-    @property
-    def parameter_names(self):
-        """The names of the parameters for this model."""
-        return self._parameter_names
 
     def compute_bandflux(self, times, filter, state):
         """Evaluate the model at the passband level for a single, given graph state and filter.
@@ -223,9 +225,13 @@ class PyLIMAWrapperModel(BandfluxModel, CiteClass):
 
         # Create the model instance.
         model = self._model_class(
-            event=current_event,
-            blend_flux_parameter=self.blend_flux_parameter,
+            current_event,
             parallax=[self.parallax_model, t0_jd],
+            blend_flux_parameter=self.blend_flux_parameter,
+            double_source=self.double_source,
+            orbital_motion=self.orbital_motion,
+            origin=self.origin,
+            fancy_parameters=self.fancy_parameters,
         )
 
         # Get the expected parameter mapping and create the ordered parameter list.
