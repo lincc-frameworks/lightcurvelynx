cc47d9e8458918688e62bb205cfda7a498592eee
Jeremy Kubica <104161096+jeremykubica@users.noreply.github.com>
2026-02-11 11:01:57 -0500

    More implementation

--- src/lightcurvelynx/obstable/argus_obstable.py
+++ src/lightcurvelynx/obstable/argus_obstable.py
@@ -1,5 +1,9 @@
 """The ArgusObsTable stores observation information from the Argus survey."""
 
+import logging
+
+import numpy as np
+from mocpy import MOC
 
 from lightcurvelynx.obstable.obs_table import ObsTable
 
@@ -14,10 +18,10 @@ https://argus.unc.edu/specifications
 """
 
 
-class ArgusObsTable(ObsTable):
-    """An ObsTable for observations from the Argus survey.
+class ArgusHealpixObsTable(ObsTable):
+    """An ObsTable for observations from the Argus survey in healpix format.
 
-    Unlike other ObsTable classes, the ArgusObsTable does not consist of a table of
+    Unlike other ObsTable classes, the ArgusHealpixObsTable does not consist of a table of
     pointings, but rather is organized at the healpix level. Each row corresponds
     to a healpix pixel and time.
 
@@ -65,23 +69,36 @@ class ArgusObsTable(ObsTable):
         "survey_name": "Argus",
     }
 
-    # Class constants for the column names.
     def __init__(
         self,
         table,
+        *,
         colmap=None,
+        apply_saturation=True,
         saturation_mags=None,
+        nside=None,
         **kwargs,
     ):
-        pass
+        # Set some default values.
+        self._spatial_data = None
+        self._nside = nside
+        self._depth = None
+
+        # Check the unsupported terms in the kwargs and raise an error if they are provided.
+        if "detector_footprint" in kwargs or "wcs" in kwargs:
+            raise ValueError("ArgusObsTable does not support detector footprints.")
+
+        super().__init__(
+            table=table,
+            colmap=colmap,
+            apply_saturation=apply_saturation,
+            saturation_mags=saturation_mags,
+            **kwargs,
+        )
 
     def uses_footprint(self):
         """Return whether the ObsTable uses a detector footprint for filtering."""
         return False
-
-    def clear_detector_footprint(self):
-        """Clear the detector footprint, so no footprint filtering is done."""
-        self._detector_footprint = None
 
     def set_detector_footprint(self, detector_footprint, wcs=None):
         """Set the detector footprint, so footprint filtering is done.
@@ -96,6 +113,77 @@ class ArgusObsTable(ObsTable):
             a footprint is provided as a Astropy region.
         """
         raise NotImplementedError("ArgusObsTable does not support detector footprints.")
+
+    def _build_spatial_data(self):
+        """Construct a mapping of healpix id to row number from the ObsTable."""
+        if self._nside is None:
+            if "nside" in self._table.colnames:
+                self._nside = self._table["nside"][0]
+            else:
+                raise ValueError(
+                    "nside must be provided for ArgusHealpixObsTable construction or "
+                    "as a column in the table."
+                )
+
+        # Check all nside values are the same.
+        if "nside" in self._table.colnames:
+            nside = self._table["nside"].to_numpy()
+            if not np.all(nside == self._nside):
+                raise ValueError(
+                    "Inconsistent nside values found in the table. Expected all nside values"
+                    f"to be {self._nside}, but found values: {np.unique(nside)}"
+                )
+
+        # Compute the depth.
+        self._depth = int(np.log2(self._nside))
+
+        self._spatial_data = {}
+        if "healpix" in self._table.colnames:
+            index = self._table["healpix"].to_numpy()
+        elif "healpix_id" in self._table.colnames:
+            index = self._table["healpix_id"].to_numpy()
+        elif self._table.index.name == "healpix_id" or self._table.index.name == "healpix":
+            index = self._table.index.to_numpy()
+        else:
+            raise ValueError(
+                "No healpix id column found in the table. Expected one of 'healpix', "
+                "'healpix_id', or index named 'healpix_id' or 'healpix'."
+            )
+
+        # Build a mapping of healpix id to row number.
+        for idx in np.unique(index):
+            self._spatial_data[idx] = np.where(index == idx)[0]
+
+    def build_moc(self, max_depth=None, **kwargs):
+        """Build a Multi-Order Coverage Map from the regions in the data set.
+
+        These are built directly from the healpix pixels.
+
+        Parameters
+        ----------
+        max_depth : int, optional
+            The maximum depth of the MOC. Default is the depth of the healpix pixels
+            in the table.
+        **kwargs : dict
+            Additional keyword arguments to pass to the MOC construction. Not currently used,
+            but accepted for consistency with the ObsTable interface.
+
+        Returns
+        -------
+        MOC
+            The Multi-Order Coverage Map constructed from the data set.
+        """
+        if max_depth is None:
+            max_depth = self._depth
+
+        logger = logging.getLogger(__name__)
+        logger.debug(f"Building MOC from ArgusHealpixObsTable at depth={max_depth}.")
+        moc = MOC.from_healpix_cells(
+            healpix_cells=np.array(list(self._spatial_data.keys())),
+            depth=self._depth,
+            max_depth=max_depth,
+        )
+        return moc
 
     def _assign_zero_points(self):
         """Assign instrumental zero points in nJy (which produces 1 e-) to the LSSTObsTable tables."""
@@ -117,3 +205,33 @@ class ArgusObsTable(ObsTable):
             Simulated bandflux noise in nJy.
         """
         raise NotImplementedError("ArgusObsTable does not have a noise model implemented yet.")
+
+    def range_search(self, query_ra, query_dec, *, radius=None, t_min=None, t_max=None):
+        """Return the indices of the pointings that fall within the field
+        of view of the query point(s).
+
+        Note that radius is not used since everything is already stored at the healpix level,
+        but it is accepted as a parameter for consistency with the ObsTable interface.
+
+        Parameters
+        ----------
+        query_ra : float or numpy.ndarray
+            The query right ascension (in degrees).
+        query_dec : float or numpy.ndarray
+            The query declination (in degrees).
+        radius : float or None, optional
+            Not used in this implementation since the data is already organized at the healpix level.
+        t_min : float, numpy.ndarray or None, optional
+            The minimum time (in MJD) for the observations to consider.
+            If None, no time filtering is applied.
+        t_max : float, numpy.ndarray or None, optional
+            The maximum time (in MJD) for the observations to consider.
+            If None, no time filtering is applied.
+
+        Returns
+        -------
+        inds : list[int] or list[numpy.ndarray]
+            Depending on the input, this is either a list of indices for a single query point
+            or a list of arrays (of indices) for an array of query points.
+        """
+        raise NotImplementedError("Range search is not implemented for ArgusHealpixObsTable yet.")
