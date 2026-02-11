Fix a few precision errors

tests/lightcurvelynx/utils/test_bicubic_interp.py
@@ -41,6 +41,11 @@ def test_bicubic_range():
     assert xvals.num_vals == 100
     assert xvals.step_size == pytest.approx(0.1)
 
+    # Depending on the version of JAX and whether other tests have set the precision,
+    # the step size might be slightly different than 0.1, which gives slightly different
+    # in some cases. So we force step size to exactly 0.1.
+    xvals.step_size = 0.1
+
     # Test that we can run out_of_bounds
     values = [-1.0, 2.0, 11.0, 5.0, -0.1, 3.5, 4.0]
     expected = [True, False, True, False, True, False, False]
@@ -57,8 +62,11 @@ def test_bicubic_range():
     ix = xvals.find_indices(x_q)
     assert jnp.all(ix >= 0)
     assert jnp.all(ix <= n_x - 2)
-    assert jnp.all((ix == n_x - 2) | (x_q < xvals.values[ix + 1]))
-    assert jnp.all((ix == 0) | (xvals.values[ix] <= x_q))
+
+    # Test that the indices are correct (using a small tolerance to account for
+    # floating point issues).
+    assert jnp.all((ix == n_x - 2) | (x_q < xvals.values[ix + 1] + 1e-6))
+    assert jnp.all((ix == 0) | (xvals.values[ix] - 1e-6 <= x_q))
 
     # Test that we fail for invalid BicubicAxis range objects.
     with pytest.raises(ValueError):
@@ -146,12 +154,6 @@ def test_bicubic_interpolator():
     # Do the JAX interpolation.
     interp_obj = BicubicInterpolator(x_vals, y_vals, z_vals)
     res_jax = interp_obj(x_q, y_q)
-
-    for i in range(len(x_q)):
-        for j in range(len(y_q)):
-            if jnp.abs(res_jax[i, j] - expected[i, j]) > 1e-5:
-                print(f"TEST {i}, {j} ({x_q[i]}, {y_q[j]}) = {res_jax[i, j]} vs {expected[i, j]}")
-
     assert jnp.allclose(expected, res_jax, rtol=1e-3, atol=1e-4)
 
 