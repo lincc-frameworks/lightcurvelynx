f3826ecb643b7fb3ba6749099245c81aa789150d
Jeremy Kubica <104161096+jeremykubica@users.noreply.github.com>
2026-03-25 11:00:46 -0400

    Change approach for randomization

--- src/lightcurvelynx/astro_utils/snia_utils.py
+++ src/lightcurvelynx/astro_utils/snia_utils.py
@@ -1,5 +1,3 @@
-from os import urandom
-
 import astropy.units as u
 import numpy as np
 import scipy.integrate as integrate
@@ -9,6 +7,7 @@ from scipy.stats import norm
 from scipy.stats.sampling import NumericalInversePolynomial
 
 from lightcurvelynx.base_models import FunctionNode
+from lightcurvelynx.math_nodes.np_random import NumpyRandomFunc
 from lightcurvelynx.math_nodes.scipy_random import NumericalInversePolynomialFunc
 
 
@@ -358,21 +357,35 @@ class SNCoordGivenPhysicalSep(FunctionNode):
         The Hubble constant.
     Omega_m : constant
         The matter density Omega_m.
+    pos_angle : parameter or None
+        The position angle for the SN location relative to the host galaxy (in radians).
+        If None, a random position angle is generated for each sample.
     **kwargs : dict, optional
         Any additional keyword arguments.
     """
 
-    def __init__(self, host_ra, host_dec, physical_sep_kpc, redshift, H0=73.0, Omega_m=0.3, seed=None, **kwargs):
+    def __init__(
+        self,
+        host_ra,
+        host_dec,
+        physical_sep_kpc,
+        redshift,
+        *,
+        H0=73.0,
+        Omega_m=0.3,
+        pos_angle=None,
+        **kwargs,
+    ):
         # Create the cosmology once for this node.
         if not isinstance(H0, float) or not isinstance(Omega_m, float):  # pragma: no cover
             raise ValueError("H0 and Omega_m must be constants.")
         self.cosmo = FlatLambdaCDM(H0=H0, Om0=Omega_m)
 
-        # Get a default random number generator for this object, using the
-        # given seed if one is provided.
-        if seed is None:
-            seed = int.from_bytes(urandom(4), "big")
-        self._rng = np.random.default_rng(seed=seed)
+        # The _sn_coord function uses a position angle that we want to randomly
+        # generate for each sample. If the user does not provide their own setter
+        # we use a numpy random function to generate the position angle [0., 2*pi].
+        if pos_angle is None:
+            pos_angle = NumpyRandomFunc("uniform", low=0.0, high=2 * np.pi)
 
         # Call the super class's constructor with the needed information.
         super().__init__(
@@ -381,6 +394,7 @@ class SNCoordGivenPhysicalSep(FunctionNode):
             host_dec=host_dec,
             physical_sep_kpc=physical_sep_kpc,
             redshift=redshift,
+            pos_angle=pos_angle,
             outputs=["ra", "dec"],
             **kwargs,
         )
@@ -407,7 +421,7 @@ class SNCoordGivenPhysicalSep(FunctionNode):
 
         return angular_sep_rad
 
-    def _sn_coord(self, host_ra, host_dec, physical_sep_kpc, redshift, rng_info=None):
+    def _sn_coord(self, host_ra, host_dec, physical_sep_kpc, redshift, pos_angle):
         """
         Function to generate SN coordinates given the host coordinates and angular separation.
 
@@ -421,9 +435,8 @@ class SNCoordGivenPhysicalSep(FunctionNode):
             The physical host-sn separation(s) in kpc.
         redshift : float or numpy.ndarray
             The redshift to convert physical separation to angular separation.
-        rng_info : numpy.random._generator.Generator or None, optional
-            A given numpy random number generator to use for this computation. If not
-            provided, the function uses the node's random number generator.
+        pos_angle : float or numpy.ndarray
+            The position angle(s) for the SN location relative to the host galaxy (in radians).
 
         Returns
         -------
@@ -432,13 +445,9 @@ class SNCoordGivenPhysicalSep(FunctionNode):
         dec : float or numpy.ndarray
             SN DEC values (in degrees).
         """
-
-        rng = rng_info if rng_info is not None else self._rng
         self.angular_sep_rad = self._host_sn_angular_separation(physical_sep_kpc, redshift)
         center = SkyCoord(host_ra * u.deg, host_dec * u.deg, frame="icrs")
-        rand_pa = 2 * np.pi * rng.random(center.size) * u.rad  # random position angle
-
-        sn_coord = center.directional_offset_by(rand_pa, self.angular_sep_rad)
+        sn_coord = center.directional_offset_by(pos_angle * u.rad, self.angular_sep_rad)
         ra = sn_coord.ra.deg
         dec = sn_coord.dec.deg
 
