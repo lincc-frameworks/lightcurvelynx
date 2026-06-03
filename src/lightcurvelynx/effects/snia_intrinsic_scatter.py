4b570dc2f5b3251c371a6cc34db7e00a5ea90eda
Mi Dai <mdai23443@gmail.com>
2026-06-03 15:16:16 -0400

    WIP

--- src/lightcurvelynx/effects/snia_intrinsic_scatter.py
+++ src/lightcurvelynx/effects/snia_intrinsic_scatter.py
@@ -1,10 +1,21 @@
 """Intrinsic scatter for SN Ia"""
 
+import os
 from pathlib import Path
 
 import numpy as np
 import sncosmo
 from lightcurvelynx.effects.effect_model import EffectModel
+
+_C11_COVARIANCE = np.array([
+    [+1.00, -0.12, -0.77, -0.91, -0.22],
+    [-0.12, +1.00, +0.57, -0.24, -0.89],
+    [-0.77, +0.57, +1.00, +0.53, -0.40],
+    [-0.91, -0.24, +0.53, +1.00, +0.49],
+    [-0.22, -0.89, -0.40, +0.49, +1.00],
+])
+
+_C11_DIAG = np.array([0.06, 0.04, 0.05, 0.04, 0.08])
 
 class SNIaIntrinsicScatter(EffectModel):
     """An effect model for intrinsic scatter in SN Ia.
@@ -17,32 +28,30 @@ class SNIaIntrinsicScatter(EffectModel):
 
     def __init__(self, modelpars, **kwargs):
         super().__init__(**kwargs)
-        self.add_effect_parameter("modelpars", modelpars)
+        self.modelpars = modelpars
 
-    def _read_g10_coefficients(self):
-        # This function reads in the polynomial coefficients for the G10 model from Guy et al. (2010). 
-        salt2_source = sncosmo.SALT2Source()
-        color_dispersion_file = Path(salt2_source.modeldir) / Path(salt2_source.cdfile)
-        g10_coefficients = np.loadtxt(color_dispersion_file, skiprows=1)
-        return g10_coefficients  
+    def _read_g10_color_dispersion_grid(self, sourcename="salt2"):
+        # This function reads in the color dispersion for the G10 model from Guy et al. (2010). 
+        modeldir = "/Users/mi/.astropy/cache/sncosmo/models/salt2/salt2-4"
+        color_dispersion_file = os.path.join(modeldir, "salt2_color_dispersion.dat")
+        g10_color_dispersion_grid = np.loadtxt(color_dispersion_file, unpack=True)
+        return g10_color_dispersion_grid
 
-    def get_intrinsic_scatter_covariance(self, wavelengths, modelpars=None):
-        if modelpars is None:
-            modelpars = {"modelname": "COH", "sigma": 0.1}
-
-        if modelpars["modelname"] == "COH":
+    def get_intrinsic_scatter_covariance(self, wavelengths):
+    
+        if self.modelpars["modelname"] == "COH":
             # This should act as gray dust, with the same scatter at all wavelengths.
-            return modelpars["sigma"] ** 2 * np.eye(len(wavelengths))
-        elif modelpars["modelname"] == "G10":
+            sigma = self.modelpars.get("sigma", 0.1)
+            return sigma ** 2 * np.eye(len(wavelengths))
+        
+        elif self.modelpars["modelname"] == "G10":
             # This applies a wavelength-dependent scatter that follows the G10 model from Guy et al. (2010). 
-            # First we read in the polynomial coefficients for the G10 model from Guy et al. (2010). The G10 model is defined as a 4th order polynomial in wavelength.
-            g10_coefficients = self._read_g10_coefficients()
-            # Then we evaluate the polynomial at the given wavelengths to get the scatter at each wavelength.
-            scatter_at_wavelengths = np.polyval(g10_coefficients, wavelengths)
-            # Finally, we construct the covariance matrix as a diagonal matrix with the scatter at each wavelength on the diagonal.
-            return np.diag(scatter_at_wavelengths ** 2)
+            g10_color_dispersion_grid = self._read_g10_color_dispersion_grid(self.modelpars.get("sourcename", "salt2"))
+            g10_color_dispersion = np.interp(wavelengths, g10_color_dispersion_grid[0], g10_color_dispersion_grid[1])   
+            coh_sigma = self.modelpars.get("coh_sigma", 0.09)
+            return coh_sigma ** 2 * np.eye(len(wavelengths)) + np.diag(g10_color_dispersion ** 2)
             
-        elif modelpars["modelname"] == "C11":
+        elif self.modelpars["modelname"] == "C11":
             # This applies a wavelength-dependent scatter that follows the C11 model from Chotard et al. (2011). 
             pass
         else:
@@ -53,7 +62,6 @@ class SNIaIntrinsicScatter(EffectModel):
         flux_density,
         times=None,
         wavelengths=None,
-        modelpars=None,
         **kwargs,
     ):
         """Apply the effect to observations (flux_density values).
@@ -77,8 +85,8 @@ class SNIaIntrinsicScatter(EffectModel):
         flux_density : numpy.ndarray
             A length T x N matrix of flux densities after the effect is applied (in nJy).
         """
-        intrinsic_scatter_covariance = self.get_intrinsic_scatter_covariance(wavelengths, modelpars=modelpars)
+        intrinsic_scatter_covariance = self.get_intrinsic_scatter_covariance(wavelengths)
 
         # Add Gaussian noise with mean 0 and standard deviation intrinsic_scatter_sigma to each flux density value
         flux_scatter = np.random.multivariate_normal(mean=np.zeros(flux_density.shape[1]), cov=intrinsic_scatter_covariance, size=flux_density.shape[0])
-        return flux_density + flux_scatter
+        return flux_density * np.power(10, -0.4 * flux_scatter)
--- tests/lightcurvelynx/effects/test_snia_intrinsic_scatter.py
+++ tests/lightcurvelynx/effects/test_snia_intrinsic_scatter.py
@@ -17,23 +17,24 @@ def test_coh_scatter() -> None:
     wavelengths = np.array([4000.0, 5000.0, 6000.0])
     flux_new = coh_scatter.apply(flux, wavelengths=wavelengths, modelpars={"modelname": "COH", "sigma": 0.1})
     assert len(np.unique(flux_new)) == 15
-    assert np.sum(np.abs(flux_new - 100.0) <= 0.5) >= 14
+    assert np.sum(np.abs(-2.5 * np.log10(flux_new/ 100.0)) <= 0.5) >= 14
 
     # We can override the default value using the parameters.
     flux_new = coh_scatter.apply(flux, wavelengths=wavelengths, modelpars={"modelname": "COH", "sigma": 1.0})
     assert len(np.unique(flux_new)) == 15
-    assert np.sum(np.abs(flux_new - 100.0) <= 5.0) >= 14
+    assert np.sum(np.abs(-2.5 * np.log10(flux_new / 100.0)) <= 5.0) >= 14
 
 def test_g10_scatter() -> None:
     """Test that we can apply G10 intrinsic scatter."""
     g10_scatter = SNIaIntrinsicScatter(modelpars={"modelname": "G10"})
 
+    assert g10_scatter.modelpars["modelname"] == "G10"
+
     # We can apply the scatter.
     flux = np.full((5, 3), 100.0)
-    wavelengths = np.array([4000.0, 5000.0, 6000.0])
+    wavelengths = np.array([3000.0, 5000.0, 8000.0])
     flux_new = g10_scatter.apply(flux, wavelengths=wavelengths, modelpars={"modelname": "G10"})
     assert len(np.unique(flux_new)) == 15
-    assert np.sum(np.abs(flux_new - 100.0) <= 0.5) >= 14
 
 def test_c11_scatter() -> None:
     """Test that we can apply C11 intrinsic scatter."""
@@ -44,4 +45,3 @@ def test_c11_scatter() -> None:
     wavelengths = np.array([4000.0, 5000.0, 6000.0])
     flux_new = c11_scatter.apply(flux, wavelengths=wavelengths, modelpars={"modelname": "C11"})
     assert len(np.unique(flux_new)) == 15
-    assert np.sum(np.abs(flux_new - 100.0) <= 0.5) >= 14
