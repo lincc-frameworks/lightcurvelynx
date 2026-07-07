a6196924088c0025722e487ab4907ff9b59ba61c
Jeremy Kubica <104161096+jeremykubica@users.noreply.github.com>
2026-07-07 12:48:06 -0400

    Create an ObsTable that covers the full sky

---
+++ tests/lightcurvelynx/obstable/test_allsky_obstable.py
@@ -0,0 +1,57 @@
+import numpy as np
+import pytest
+from lightcurvelynx.obstable.allsky_obstable import AllSkyObsTable
+
+
+def test_create_allsky_obstable():
+    """Create a minimal AllSkyObsTable object and perform basic queries."""
+    num_points = 50
+    values = {
+        "time": 59000.0 + np.arange(num_points),
+        "filter": np.random.choice(["g", "r", "i"], size=num_points),
+    }
+    obs_table = AllSkyObsTable(values)
+    assert len(obs_table) == num_points
+
+    # The query should return all indices since the AllSkyObsTable covers the entire sky.
+    query_ra = np.random.uniform(0.0, 360.0, size=10)
+    query_dec = np.random.uniform(-90.0, 90.0, size=10)
+    inds = obs_table.range_search(query_ra, query_dec)
+    assert len(inds) == 10
+    for ind in inds:
+        assert np.array_equal(ind, np.arange(num_points))
+
+    # We can filter on time though.
+    t_min = 59010.0
+    t_max = 59020.0
+    inds = obs_table.range_search(query_ra, query_dec, t_min=t_min, t_max=t_max)
+    for ind in inds:
+        assert np.all(obs_table._table["time"].to_numpy()[ind] >= t_min)
+        assert np.all(obs_table._table["time"].to_numpy()[ind] <= t_max)
+
+    # We can filter on times that are arrays.
+    t_min = 59010.0 + np.arange(10)
+    t_max = 59020.0 + np.arange(10)
+    inds = obs_table.range_search(query_ra, query_dec, t_min=t_min, t_max=t_max)
+    for i, ind in enumerate(inds):
+        assert np.all(obs_table._table["time"].to_numpy()[ind] >= t_min[i])
+        assert np.all(obs_table._table["time"].to_numpy()[ind] <= t_max[i])
+
+    # We fail if we are missing required columns.
+    with pytest.raises(KeyError):
+        _ = AllSkyObsTable({"time": 59000.0 + np.arange(num_points)})
+    with pytest.raises(KeyError):
+        _ = AllSkyObsTable({"filter": np.random.choice(["g", "r", "i"], size=num_points)})
+
+
+def test_allsky_obstable_build_moc():
+    """Test that the MOC built from an AllSkyObsTable covers the entire sky."""
+    num_points = 50
+    values = {
+        "time": 59000.0 + np.arange(num_points),
+        "filter": np.random.choice(["g", "r", "i"], size=num_points),
+    }
+    obs_table = AllSkyObsTable(values)
+    moc = obs_table.build_moc(max_depth=5)
+    assert moc is not None
+    assert moc.sky_fraction == pytest.approx(1.0, rel=1e-6)
